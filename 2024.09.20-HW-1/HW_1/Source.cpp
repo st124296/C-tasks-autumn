f482f7f394cb9b934622a03bb55dc6e13c3ae9dd
st124296 <mollylazareva775@gmail.com>
2024-09-20 18:52:45 +0300

    HW1 2 tasks upload

---
+++ 2024.09.20-HW-1/HW_1/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int num1;
+	int num2;
+	scanf_s("%d", &num1);
+	scanf_s("%d", &num2);
+	printf("%d", num1 + num2);
+	return EXIT_SUCCESS;
+}
---
+++ 2024.09.20-HW-1/HW_2/Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int num;
+	scanf_s("%d", &num);
+	printf("%d", num);
+	return EXIT_SUCCESS;
+}
